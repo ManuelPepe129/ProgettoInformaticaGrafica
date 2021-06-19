3c265ab7ebb0890496a336053fdfbb062d61e3a7
Manuel Pepe <manuel.pepe@stud.unifi.it>
2021-06-19 21:11:03 +0200

    Enemies fix

--- ProgettoInformaticaGrafica/src/entities/enemy.cpp
+++ ProgettoInformaticaGrafica/src/entities/enemy.cpp
@@ -1,30 +1,37 @@
 #include "enemy.h"
 
 Enemy::Enemy()
-	: EntityBase("enemy", "enemy", nullptr)
+	: EntityBase("enemy", "enemy", nullptr),
+	respawnDelay(15.0f),
+	respawnTimer(respawnDelay),
+	maxSpeed(10.0f),
+	speed(2)
 {
 	
 }
 
 Enemy::Enemy(const std::string& modelId, Scene* scene)
-	:EntityBase("enemy", modelId, scene),
+	: EntityBase("enemy", modelId, scene),
 	respawnDelay(15.0f),
 	respawnTimer(respawnDelay),
-	maxSpeed(10)
+	maxSpeed(10.0f),
+	speed(2.0f)
 { }
 
-void Enemy::setPath(glm::vec3 start, glm::vec3 end, float speed)
+void Enemy::setPath(glm::vec3 start, glm::vec3 end)
 {
 	this->start = start;
 	this->end = end;
-	this->speed = speed > maxSpeed ? maxSpeed : speed;
-	rigidBody->velocity = glm::normalize(end - start)*speed;
+	this->speed = (speed > maxSpeed) ? maxSpeed : speed;
+	rigidBody->velocity = glm::normalize(end - start) * speed;
+	glm::vec3 v = rigidBody->velocity;
+	//float yaw = atan2(rigidBody->velocity.x, rigidBody->velocity.y);
+	//rigidBody->rot = glm::vec3(0.0f, (yaw), 0.0f);
 }
 
 void Enemy::init(glm::vec3 size, float mass, glm::vec3 pos)
 {
 	EntityBase::init(size, mass, pos);
-	setPath(start, end, speed++);
 }
 
 void Enemy::update(double dt)
@@ -33,7 +40,7 @@ void Enemy::update(double dt)
 	{
 		if (length(end - rigidBody->pos) <= 0.1f)
 		{
-			setPath(end, start, speed++);
+			setPath(end, start);
 		}
 	}
 	else
@@ -43,6 +50,8 @@ void Enemy::update(double dt)
 		{
 			respawnTimer = respawnDelay;
 			init(glm::vec3(1.0f), 1.0f, start);
+			speed++;
+			setPath(end, start);
 		}
 	}
 	
--- ProgettoInformaticaGrafica/src/entities/enemy.h
+++ ProgettoInformaticaGrafica/src/entities/enemy.h
@@ -8,7 +8,7 @@ public:
 	Enemy();
 	Enemy(const std::string& modelId, Scene* scene);
 
-	void setPath(glm::vec3 start, glm::vec3 end, float speed = 2.0f);
+	void setPath(glm::vec3 start, glm::vec3 end);
 
 	virtual void init(glm::vec3 size, float mass, glm::vec3 pos);
 	virtual void update(double dt) ;
